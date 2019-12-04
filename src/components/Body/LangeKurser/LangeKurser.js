6a882c75e03aad890b30b50edce0c6199b8f0b61
Cabrina <cabrinaengedal@outlook.com>
2019-12-04 13:45:16 +0100

    styling

--- src/components/Body/LangeKurser/LangeKurser.js
+++ src/components/Body/LangeKurser/LangeKurser.js
@@ -5,6 +5,7 @@ export default function LangeKurser() {
         <div className="langekurser">
             <h1>LANGE KURSER – FORLØB</h1>
             <div className="kursusliste">
+                <h2>kommende elevhold</h2>
                 <ul>
                     <li>Efterårs-semester 2019, 18. august til 14. december 2019, 17 uger</li>
                     <li>Efterårs-semester 2019, 15. september til 14. december 2019, 13 uger</li>
@@ -12,6 +13,30 @@ export default function LangeKurser() {
                     <li>Forårs-semester 2020, 23. februar til 13. juni 2020, 16 uger</li>
                 </ul>  
             </div>
+            <div className="lkurser-content">
+                <div>
+                    <h2>De første dage på højskolen</h2>
+                    <p>I dine første dage på Den lille højskole bruger vi tiden på at introducere alle vores linjefag og valgfag, således at du kan få valgt det skema, der passer præcis til dig. Via din tilmelding har du allerede markeret et uforpligtende valg af linjefag, men du vælger altså først definitivt dine fag i løbet af de første dage på skolen. Den almindelige skema-undervisning starter herefter onsdag eftermiddag.</p>
+                    <p>Sociale aktiviteter fylder meget i introugen, hvor vi gør rigtigt meget ud af at du hurtigt lærer dine nye højskole-kammerater godt at kende. Intro-ugen afsluttes altid med et brag af en fredags-fest, hvor du bl.a. vil opleve Nørgaards forrygende lærerband.</p>
+                </div>
+                <div>
+                    <h2>Indkvartering</h2>
+                    <p>Man indkvarteres på dobbeltværelse og får således en room-mate på sit højskoleophold. Ønsker man enkelt-værelse kan det imødekommes, men det koster ekstra (se menupunkt om priser & tilskud). Bemærk at vi har ganske få enkeltværelser til rådighed.</p>
+                </div>
+                <div>
+                    <h2>Almindeligt skema</h2>
+                    <p>Efter en tætpakket introuge, som har fået dig til at føle dig hjemme på højskolen, går de følgende uger med almindelig skemaundervisning via de mange spændende fag du har valgt. Hvis du har fortrudt nogle af dine valg ser vi gerne at du hurtigst muligt snakker med dine lærere om evt. at skifte.</p>
+                </div>
+                <div>
+                    <h2>Filmskuespil</h2>
+                    <p>Vælger du Filmskuespil skal du i løbet af dit ophold møde en række professionelle gæstelærere fra branchen (castere, instruktører m.fl.). Der er i den forbindelse en samlet merbetaling på 2.000 kr. hvis du vælger Filmskuespil.</p>
+                </div>
+                <div>
+                    <h2>Storby-studieture</h2>
+                    <p>Som elev på Den lille højskole kan du vælge mellem to studieture i løbet af dit ophold, nemlig Berlin eller New York. Berlin-turen er allerede betalt via tilmeldingsgebyret, men du har mulighed for at “opgradere” til en New York-tur mod en merbetaling på 8.500 kr. Begge ture er alt inklusive undtaget lommepenge.</p>
+                </div>
+            </div>
+            <button><i className="fas fa-check"></i>Tilmeld dig her</button>
         </div>
     )
 }
